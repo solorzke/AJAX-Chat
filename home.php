87114fa2c58c2bd510622e693d6aa1efbf74afee
Kevin Solorzano <solorzke@gmail.com>
2018-11-28 18:11:25 -0500

    Add css file <link> to head.

--- home.php
+++ home.php
@@ -6,6 +6,8 @@
 		<title>IT202 - HW5</title>
 		<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 		<script type="text/javascript" src="Js/ajax-app.js"></script>
+		<link rel="stylesheet" type="text/css" href="index.css">
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 	</head>
 	<body>
 		<div class="chat-box">
